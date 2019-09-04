a28cf3ef916444ebdb6310e280bc1d932b2f54ae
nerdylegend <31123377+nerdylegend@users.noreply.github.com>
2019-09-04 05:59:47 +0530

    Update PersonDataAccessService.java

--- src/main/java/com/nerdylegend/backend/dao/PersonDataAccessService.java
+++ src/main/java/com/nerdylegend/backend/dao/PersonDataAccessService.java
@@ -1,6 +1,8 @@
 package com.nerdylegend.backend.dao;
 
 import com.nerdylegend.backend.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,14 @@ import java.util.UUID;
 
 @Repository("postgres")
 public class PersonDataAccessService implements PersonDAO {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int insertPerson(UUID id, Person person) {
         return 0;
@@ -16,12 +26,19 @@ public class PersonDataAccessService implements PersonDAO {
 
     @Override
     public List<Person> selectAllPeople() {
-        return List.of(new Person(UUID.randomUUID(), "From POSTGRES DB"));
+        final String sql = "SELECT id, name FROM person";
+        return jdbcTemplate.query(sql, (resultSet, i) ->
+                new Person(UUID.fromString(resultSet.getString("id")), resultSet.getString("name"))
+        );
     }
 
     @Override
     public Optional<Person> selectPersonById(UUID id) {
-        return Optional.empty();
+        final String sql = "SELECT id, name FROM person where id=?";
+        Person person = jdbcTemplate.queryForObject(sql, new Object[]{id}, (resultSet, i) ->
+                new Person(UUID.fromString(resultSet.getString("id")), resultSet.getString("name"))
+        );
+        return Optional.ofNullable(person);
     }
 
     @Override
